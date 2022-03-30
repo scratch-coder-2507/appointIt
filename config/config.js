20a56bfabdfa047d66f544effdb06e03cf812713
unknown <yashmore7000@gmail.com>
2022-03-30 20:44:57 +0530

    hello

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const Port = process.env.NODE_ENV === 'test' ? 4001 : (process.env.PORT || 4000);
 
-const MongodbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URL_PRODUCTION : (process.env.MONGODB_URL_DEVELOPMENT || 'mongodb+srv://hgadmin:hardik123@cluster0.osahc.mongodb.net/appoint?authSource=admin&replicaSet=atlas-11w40y-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true')
+const MongodbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URL_PRODUCTION : (process.env.MONGODB_URL_DEVELOPMENT || 'mongodb+srv://yash:yash@123@cluster0.4syiy.mongodb.net/uppoint?retryWrites=true&w=majority')
 
 module.exports = {
     Port: Port,
